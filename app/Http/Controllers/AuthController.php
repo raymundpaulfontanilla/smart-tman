5e9a908121a61a59fa09399db3cf58670804634a
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-11-19 15:37:20 +0800

    refactor message

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AuthRequest;
+use App\Http\Requests\LoginRequest;
+use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Str;
 
@@ -31,5 +33,47 @@ class AuthController extends Controller
             'message' => 'Successfully registered account, you may now try to login.',
             'user' => $user,
         ], 201);
+    }
+
+    function login(Request $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'success' => false,
+                'statusCode' => 404,
+                'errorCode' => 'EMAIL_OR_PASSWORD_INCORRECT',
+                'message' => 'email or password is incorrect',
+                'user' => null
+            ], 404);
+        }
+
+        $salt = $user->salt;
+        $storedHash = $user->password;
+
+        $passwordWithSalt = $request->password . $salt;
+        $hashedInput = hash('sha256', $passwordWithSalt);
+
+        if ($hashedInput == $storedHash) {
+            return response()->json([
+                'success' => true,
+                'statusCode' => 200,
+                'message' => 'Successfully logged in. Welcome ' . $user->name,
+                'user' => ([
+                    'id' => $user->id,
+                    'name' => $user->name,
+                    'email' => $user->email,
+                ]),
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'statusCode' => 404,
+                'errorCode' => 'EMAIL_OR_PASSWORD_INCORRECT',
+                'message' => 'email or password is incorrect',
+                'user' => null
+            ], 404);
+        }
     }
 }
