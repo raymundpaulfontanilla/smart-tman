dc6e99027284aa908423919a95dc0ece9bd72924
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-11-21 02:51:31 +0800

    add laravel has many relationship to project model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -62,4 +63,9 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
+
+    public function projects(): HasMany
+    {
+        return $this->hasMany(Project::class);
+    }
 }
