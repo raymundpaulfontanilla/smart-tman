83d9d058ff469b7d510a865614a31b01e498a294
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-11-21 05:09:23 +0800

    re arrange index route and store route

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,8 @@ Route::prefix('accounts')->group(function () {
 });
 
 Route::prefix('projects')->group(function () {
-    Route::get('/', [ProjectController::class, 'index']);
     Route::post('/create-project', [ProjectController::class, 'store']);
+    Route::get('/', [ProjectController::class, 'index']);
     Route::get('/{id}', [ProjectController::class, 'show']);
     Route::put('/{id}', [ProjectController::class, 'update']);
     Route::delete('/{id}', [ProjectController::class, 'destroy']);
