fbd5338e5678229aa4fda8a2ada2a11da96807d4
Raymund Fontanilla <raymundpaulfontanilla@gmail.com>
2025-11-21 02:53:17 +0800

    add route for create project

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('accounts')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::prefix('project')->group(function () {
+    Route::post('/create-project', [ProjectController::class, 'store']);
 });
